e8296e9b64b38a5f3286f5edfa4bebcb68bbe818
fradin.emma <fradin.emma@appirits.com>
2024-06-07 14:00:31 +0900

    fadein effect

--- js/index.js
+++ js/index.js
@@ -47,6 +47,21 @@ document.addEventListener("DOMContentLoaded", function () {
         tabContent[index].classList.add('is-shown');
       };
     }
-    
   }
+
+  let elementsArray = document.querySelectorAll(".ly-section");
+  window.addEventListener('scroll', fadeIn ); 
+  function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
+    }
+  }
+  fadeIn();
+
 })
